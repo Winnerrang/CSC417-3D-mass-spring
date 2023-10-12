b1b3d4453d6744c927e4bdc8092585c094a6e43e
Winnerrang <leo.hanxu@mail.utoronto.ca>
2023-10-12 01:00:02 -0400

    Finish Simulation initialization

--- src/mass_matrix_particles.cpp
+++ src/mass_matrix_particles.cpp
@@ -1,6 +1,9 @@
 #include <mass_matrix_particles.h>
-
+#include <iostream>
 void mass_matrix_particles(Eigen::SparseMatrixd &M, Eigen::Ref<const Eigen::VectorXd> q, double mass) {
 
-    
+	M.resize(q.rows(), q.rows());
+	M.setIdentity();
+	M *= mass;
+
 }
